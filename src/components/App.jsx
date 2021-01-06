ac62d02d07262e65cdc6a91477d7de17410c4278
fteohh <67304099+fteohh@users.noreply.github.com>
2021-01-05 22:16:46 -0600

    completed section1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import notes from "../notes";
+import Header from './Header';
+import Footer from './Footer';
+import Note from './Note';
 
 function App() {
   return (
     <div>
-    <h1></h1>
+      <Header />
+      <Footer />
+      <Note />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import notes from "../notes";
+
+
+function Footer() {
+    return (
+        <div>
+            <footer>
+                <p>Copyright&#169;2020</p>
+            </footer>
+        </div>
+    )
+}
+export default Footer;
